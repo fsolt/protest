e6c8015e03e211f6188262fb1bc979fd79a9ad41
Frederick Solt <fsolt@mac.com>
2016-08-31 13:00:17 -0500

    color by year, plot by quartile

--- R/cs_plot.R
+++ R/cs_plot.R
@@ -84,7 +84,7 @@ pdf("paper/figures/cs.pdf")
 align_plots(p1a, p1b)
 dev.off()
 
-# With year colors
+# With year colors, by quartile
 
 p1_data <- a_res %>%    
     group_by(country) %>% 
@@ -92,26 +92,66 @@ p1_data <- a_res %>%
     ungroup() %>%
     arrange(-estimate) %>%
     filter(ub - lb < .3) %>% 
-    mutate(half = ntile(estimate, 2),
+    mutate(quarter = ntile(estimate, 4),
            ranked = row_number(estimate),
-           yr = year)
-p1_data$yr <- car::recode(p1_data$yr, "2001:2010 = 'Before 2011'")
-p1_data$yr <- factor(p1_data$yr, levels = c("Before 2011", "2011", "2012", "2013", "2014", "2015"))
+           Year = year)
+p1_data$Year <- car::recode(p1_data$Year, "2001:2010 = 'Before 2011'")
+p1_data$Year <- factor(p1_data$Year, levels = c("Before 2011", "2011", "2012", "2013", "2014", "2015"))
 
-p1_data_a <- p1_data %>% filter(half==2)
+p1_data_a <- p1_data %>% filter(quarter==4)
+p1_data_b <- p1_data %>% filter(quarter==3)
 
 p1a <- ggplot(p1_data_a, aes(x = estimate,
-                             y = row_number(estimate), colour=yr)) +
+                             y = row_number(estimate), colour=Year)) +
     geom_point(na.rm = TRUE) +
     theme_bw() + 
-    theme(legend.position="none") +
+    theme(legend.position=c(1, 0),
+          legend.justification = c(1, 0),
+          axis.text.x  = element_text(size=7),
+          axis.text.y  = element_text(size=7),
+          axis.title.x = element_text(face="bold", size=7),
+          legend.text = element_text(size = 7),
+          legend.title = element_text(size=7, face = "bold"),
+          legend.key.size = unit(.5, "line"),
+          legend.background = element_rect(linetype = "solid",
+                                           color = "grey80",
+                                           size = .25),
+          legend.key = element_rect(colour = "white")) +
     geom_segment(aes(x = lb,
                      xend = ub,
                      y = row_number(estimate), yend = row_number(estimate),
-                     colour=yr), na.rm = TRUE) +
+                     colour=Year), na.rm = TRUE) +
     scale_y_continuous(breaks = row_number(p1_data_a$estimate), labels=p1_data_a$country) +
     coord_cartesian(xlim=c(0, .6)) +
-    ylab("") + xlab("")
+    ylab(NULL) + xlab("Protest") +
+    scale_color_grey(start = .8, end = 0)
+ggsave("paper/figures/cs_1q.pdf", width = 6, height = 4)
+
+p1b <- ggplot(p1_data_b, aes(x = estimate,
+                             y = row_number(estimate), colour=Year)) +
+    geom_point(na.rm = TRUE) +
+    theme_bw() + 
+    theme(legend.position=c(1, 0),
+          legend.justification = c(1, 0),
+          axis.text.x  = element_text(size=7),
+          axis.text.y  = element_text(size=7),
+          axis.title.x = element_text(face="bold", size=7),
+          legend.text = element_text(size = 7),
+          legend.title = element_text(size=7, face = "bold"),
+          legend.key.size = unit(.5, "line"),
+          legend.background = element_rect(linetype = "solid",
+                                           color = "grey80",
+                                           size = .25),
+          legend.key = element_rect(colour = "white")) +
+    geom_segment(aes(x = lb,
+                     xend = ub,
+                     y = row_number(estimate), yend = row_number(estimate),
+                     colour=Year), na.rm = TRUE) +
+    scale_y_continuous(breaks = row_number(p1_data_b$estimate), labels=p1_data_b$country) +
+    coord_cartesian(xlim=c(0, .6)) +
+    ylab(NULL) + xlab("Protest") +
+    scale_color_grey(start = .8, end = 0)
+ggsave("paper/figures/cs_2q.pdf", width = 6, height = 4)
 
 
 # Split by quarters
