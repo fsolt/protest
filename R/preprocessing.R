c5d40cb5d7192d2d96eda069765dff0facb1888d
Frederick Solt <fsolt@mac.com>
2016-09-01 22:17:47 -0400

    omit least demanding activities

--- R/preprocessing.R
+++ R/preprocessing.R
@@ -16,6 +16,11 @@ prot <- dcpo_setup(prot0)
 write_csv(prot, "data/all_data_prot.csv")
 
 prot <- read_csv("data/all_data_prot.csv")
-# prot_a <- prot %>% filter(cc_rank>=10 & firstyr!=lastyr) %>%
-#   mutate(ccode = as.numeric(factor(ccode)))
-
+prot_a <- prot %>%
+    mutate(vals = str_extract(variable, "[23]") %>% as.numeric(),
+           p = y_r/n) %>% 
+    filter(!(vals==3 & cutpoint==1 & variable!="demo_year3")) %>% 
+    filter(!str_detect(variable, "petit")) %>% 
+    filter(!str_detect(variable, "boycott"))
+prot0 <- prot
+prot <- prot_a %>% mutate(rcode = as.numeric(factor(rcode, levels = unique(rcode))))
